e7981d8e1ab9240231e5cf2516bce20f661eaede
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2021-06-06 12:11:45 +0200

    init

---
+++ src/test/java/com/the_register/TheRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.the_register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
