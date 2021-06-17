c5030c4d99a41415c40e2957cf98f8443dc188eb
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2021-06-17 20:57:16 +0200

    implemented theRegister class

---
+++ src/main/java/com/the_register/theRegister.java
@@ -0,0 +1,11 @@
+package com.the_register;
+
+import java.time.LocalDate;
+
+public class theRegister {
+    private long id;
+    private LocalDate date;
+    private Place_type place_type;
+    private String desc;
+    private double numberOfHours;
+}
